98766ebef32290d56b52e63682ec010a3cdc73c6
cairenhe <ciarenhe00@163.com>
2019-01-29 23:01:03 +0800

    发布

--- Log4jtemplate/src/main/java/cn/cairenhe/log4j/Template.java
+++ Log4jtemplate/src/main/java/cn/cairenhe/log4j/Template.java
@@ -1,7 +1,10 @@
 package cn.cairenhe.log4j;
+import java.util.logging.Logger;
 
 public class Template {
     public static void main(String args[]){
+        Logger logger = Logger.getLogger("console");
+        logger.info("haha");
 
     }
 }
